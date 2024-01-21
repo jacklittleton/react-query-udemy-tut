4a05a8b1f896b1e48ff259ae33496e09d5ae159f
Jack Littleton <jack.littleton@gmail.com>
2024-01-20 21:24:35 -0800

    Add pagination

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -7,13 +7,13 @@ import { useQuery } from "@tanstack/react-query";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
   const { data, isError, error, isLoading, errorUpdateCount } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
     staleTime: 5000,
   });
 
@@ -45,16 +45,20 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1} onClick={() => {
+          setCurrentPage((previousValue) => previousValue - 1);
+        }}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage} onClick={() => {
+          setCurrentPage((previousValue) => previousValue + 1);
+        }}>
           Next page
         </button>
       </div>
-      <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      <hr/>
+      {selectedPost && <PostDetail post={selectedPost}/>}
     </>
   );
 }
